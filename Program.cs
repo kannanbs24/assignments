ec490fa933ecc48f8af7a7b60933f905eb0d3441
kannanbs24 <111351861+kannanbs24@users.noreply.github.com>
2022-08-20 17:22:02 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -175,3 +175,21 @@ value = Convert.ToDouble(Console.ReadLine());
 
 inr = usd * value;
 Console.WriteLine("USD " + usd + "-> INR " + inr);  */  //15 
+
+/* string first_name;
+string last_name;
+Console.WriteLine(  "enter the first name");
+first_name = Console.ReadLine();
+Console.WriteLine("enter the last name");
+last_name = Console.ReadLine();
+if(first_name=="userid" && last_name=="password")
+{
+    Console.WriteLine("user has succesfully login in");
+    Console.WriteLine("welcome to my app");
+}
+else
+{
+    Console.WriteLine("incorrect first_name and password");
+}
+
+*/                    //16
