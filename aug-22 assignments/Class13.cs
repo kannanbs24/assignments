3dea9b80a88c45054cb9b13660eab43a2044b077
kannan <kannanbsk1609080@gmail.com>
2022-08-22 22:22:40 +0530

    uploads

--- aug-22 assignments/Class13.cs
+++ aug-22 assignments/Class13.cs
@@ -8,9 +8,37 @@ namespace aug_22_assignment
 {
     internal class Class13
     {
-        static void Main(string[] args)
+        static int count(int[] S, int m, int n)
         {
+            if (n == 0)
+                return 1;
 
+
+            if (n < 0)
+                return 0;
+
+            if (m <= 0)
+                return 0;
+
+
+            return count(S, m - 1, n) +
+                count(S, m, n - S[m - 1]);
+        }
+        public static void Main()
+        {
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m = ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("arr = ");
+            int[] arr = new int[n];
+            var stringArray = Console.ReadLine().Split(' ');
+            for (var i = 0; i < m; i++)
+            {
+                arr[i] = int.Parse(stringArray[i]);
+            }
+            Console.WriteLine("Output = {0}", count(arr, m, n));
+            Console.ReadLine();
         }
     }
 }
---
+++ aug-22 assignments/Class14.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aug_22_assignment
+{
+    internal class Class14
+    {
+        public static void printFirstRepeating(int[] arr)
+        {
+            int min = -1;
+
+            // Creates an empty hashset
+            HashSet<int> set = new HashSet<int>();
+
+            // Traverse the input array from right to left
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                // If element is already in hash set, update min
+                if (set.Contains(arr[i]))
+                {
+                    min = i;
+                }
+
+                else // Else add element to hash set
+                {
+                    set.Add(arr[i]);
+                }
+
+            }
+
+            // Print the result
+            if (min != -1)
+            {
+                Console.WriteLine("The first repeating element is " + arr[min]);
+            }
+            else
+            {
+                Console.WriteLine("There are no repeating elements");
+            }
+            Console.ReadLine();
+
+        }
+
+
+        // Driver method to test above method
+
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 10, 5, 3, 4, 3, 5, 6 };
+            printFirstRepeating(arr);
+        }
+    }
+}
