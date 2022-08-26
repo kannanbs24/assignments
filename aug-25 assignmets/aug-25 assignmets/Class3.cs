ed23e06e7709cbd9194f72d2038e014626d1d053
kannan <kannanbsk1609080@gmail.com>
2022-08-26 13:06:39 +0530

    uploadssss

---
+++ aug-25 assignmets/aug-25 assignmets/Class3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aug_25_assignmets
+{
+    internal class Class3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C drive info");
+            DriveInfo info = new DriveInfo("C");
+            Console.WriteLine(info.Name);
+            Console.WriteLine(info.TotalSize);
+            Console.WriteLine(info.DriveType);
+            Console.WriteLine(info.DriveFormat);
+            Console.WriteLine("----------------------------");
+            DriveInfo[] dinfo = DriveInfo.GetDrives();
+            foreach (var item in dinfo)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
