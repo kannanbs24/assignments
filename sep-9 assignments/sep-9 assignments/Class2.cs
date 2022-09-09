698895d1e0174b49375b28c3f719fdbf1b31bd12
kannan <kannanbsk1609080@gmail.com>
2022-09-09 23:11:34 +0530

    added

---
+++ sep-9 assignments/sep-9 assignments/Class2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sep_9_assignments
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            //imterprocess
+            using (Mutex mutex = new Mutex(false, "ThreadingDemo"))
+            {
+
+                if (!mutex.WaitOne(1000, false))
+                {
+                    Console.WriteLine("already running....");
+                    Console.ReadLine();
+                    return;
+
+                }
+                else
+                {
+                    Console.WriteLine("app is running");
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
