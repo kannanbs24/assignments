a2013bb20a39d0a24a2c92daa9c6a2eeacdfceb5
kannan <kannanbsk1609080@gmail.com>
2022-09-12 22:01:50 +0530

    added

---
+++ sep-12 assignments/ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class simple
+    {
+        private double i;
+
+        public double num1
+        {
+            get { return i; }
+            set { i = value; }
+        }
+
+
+        public void cube(double i)
+        {
+            double a = i * i * i;
+           
+            Console.WriteLine("Cube of {0} = {1}", i, a);
+        }
+
+        public void sqrt(double i)
+        {
+            double s = Math.Sqrt(i);
+            Console.WriteLine("Sqrt of {0} = {1}", i, s);
+        }
+    }
+}
---
+++ sep-12 assignments/sep-12 assignmentss/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace sep_12_assignmentss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "E:\\c# assignment\\sep-12 assignmentss\\ClassLibrary1\\bin\\Debug\\ClassLibrary1.dll";
+            Assembly asm = Assembly.LoadFrom(path);
+            Type[] types = asm.GetTypes();
+            Type t = null;
+
+            foreach (var item in types)
+            {
+                t = asm.GetType(item.FullName);
+
+                object obj = Activator.CreateInstance(t);
+                item.InvokeMember("cube", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { 10.0 });
+                item.InvokeMember("sqrt", BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1] { 10.0 });
+
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("-------------");
+        }
+    }
+}
