2ea6134d8738b97a16e69970a41ed646df6f858e
kannan <kannanbsk1609080@gmail.com>
2022-09-25 20:52:08 +0530

    added

---
+++ sept-23 assignments/ClassLibrary1/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		private int productid;
+
+		public int ID
+		{
+			get { return productid; }
+			set
+			{
+				if (productid == null)
+				{
+                    throw new Exception("entry crt value");
+                }
+				else
+				{
+					productid = value;
+				}
+			}
+		}
+		private string productname;
+
+		public string Name
+		{
+			get { return productname; }
+			set {
+				if ((String.IsNullOrEmpty(value)))
+				{
+                    throw new Exception("entry crt value");
+
+                }
+				else
+				{
+					productname = value;
+				}
+			}
+
+
+		}
+		private int unitprice;
+
+		public int Price
+		{
+			get { return  unitprice; }
+			set
+			{
+				if (unitprice==null)
+				{
+
+                    throw new Exception("entry crt value");
+
+                }
+				else
+				{
+					unitprice = value;
+				}
+			}
+		}
+
+	}
+}
---
+++ sept-23 assignments/sept-23 assignments/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace sept_23_assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+            Class1 obj=new Class1();
+          //  Console.WriteLine("enter the product key to search");
+           // obj.ID=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the amount to search");
+            obj.Price=Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection("data source=.; database=kannan; integrated security=SSPI");
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("select * from product where unitprice='" + obj.Price + "'", con);
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["productid"] + " " + sdr["productname"] + " " + sdr["unitprice"]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
