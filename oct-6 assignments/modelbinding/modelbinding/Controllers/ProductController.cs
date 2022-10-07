7b22c15567789520b9b998e72884fe26a6a88db3
kannan <kannanbsk1609080@gmail.com>
2022-10-07 20:05:14 +0530

    added

---
+++ oct-6 assignments/modelbinding/modelbinding/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using modelbinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace modelbinding.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        static  List<ProductModel> prodList = new List<ProductModel>();
+        static ProductController()
+        {
+            prodList.Add(new ProductModel { Prodid = 1, Prodname = "book", ManufacturingDate = new DateTime(2021, 03, 06) });
+            prodList.Add(new ProductModel { Prodid = 2, Prodname = "pen", ManufacturingDate = new DateTime(2022, 04, 07) });
+            prodList.Add(new ProductModel { Prodid = 3, Prodname = "pencil", ManufacturingDate = new DateTime(2020, 05, 09) });
+
+
+        }
+        public ActionResult Index()    //start page
+        {
+            return View(prodList);
+        }
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductModel model)
+        {
+           prodList.Add(model);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            ProductModel foundData = prodList.Find(customer => customer.Prodid == id);
+            return View(foundData);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ProductModel foundData = prodList.Find(customer => customer.Prodid == id);
+
+
+            return View(foundData);
+        }
+
+        [HttpPost]
+
+        public ActionResult Edit(int id, ProductModel m)
+        {
+            ProductModel foundData = prodList.Find(customer => customer.Prodid == id);
+            prodList.Remove(foundData);
+            prodList.Add(m);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            ProductModel foundData = prodList.Find(customer => customer.Prodid == id);
+            return View(foundData);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, ProductModel cust)
+        {
+            ProductModel foundData = prodList.Find(customer => customer.Prodid == id);
+            prodList.Remove(foundData);
+            return View(foundData);
+        }
+
+        
+    }
+}
---
+++ oct-6 assignments/modelbinding/modelbinding/Controllers/studentController.cs
@@ -0,0 +1,24 @@
+﻿using modelbinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace modelbinding.Controllers
+{
+    public class studentController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            List<ProductModel> prodList = new List<ProductModel>();
+            prodList.Add(new ProductModel { Prodid = 1, Prodname = "book", ManufacturingDate = new DateTime(2021, 03, 06) });
+            prodList.Add(new ProductModel { Prodid = 2, Prodname = "pen", ManufacturingDate = new DateTime(2022, 04, 07) });
+            prodList.Add(new ProductModel { Prodid = 3, Prodname = "pencil", ManufacturingDate = new DateTime(2020, 05, 09) });
+
+            ViewBag.customers = prodList;
+            return View();
+        }
+    }
+}
